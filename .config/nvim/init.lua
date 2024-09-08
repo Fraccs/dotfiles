57c6adc751b41a11ea9f61af1d1c1f28cf13c710
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2024-09-08 18:46:07 +0200

    fix(nvim): set cursor to `block` in every mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,7 @@ vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+vim.opt.guicursor = "n-v-c:block,i:block"
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 30
