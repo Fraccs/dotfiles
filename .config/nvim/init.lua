7bafbbd6f4b17585487c6d3d43ee9aed6b2e88a5
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2024-10-25 15:54:34 +0200

    feat(nvim): add keymap to open `Neotree`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,6 +50,8 @@ vim.opt.scrolloff = 30 -- Minimal number of screen lines to keep above and below
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('n', '<C-f>', ':Neotree<CR>', { desc = 'Open Neotree file explorer' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
