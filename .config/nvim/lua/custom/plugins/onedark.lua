3d9dfbd3d36f8e2ea0bfb2eba86f06445b4899fa
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2024-10-27 15:31:42 +0100

    fix(neovim): modify `bg` and `fg` colors of `onedark`

--- .config/nvim/lua/custom/plugins/onedark.lua
+++ .config/nvim/lua/custom/plugins/onedark.lua
@@ -4,6 +4,13 @@ return {
   priority = 1000,
   config = function()
     require('onedark').setup {
+      colors = {
+        bg0 = '#000000',
+        bg1 = '#111111',
+        bg2 = '#353839',
+        bg3 = '#808080',
+        fg = '#ffffff'
+      },
       style = 'dark',
       transparent = true
     }
